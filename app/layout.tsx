04679494807fbb8be98d09cd780e773e6d17884a
24f2006874 <24f2006874@ds.study.iitm.ac.in>
2025-11-26 21:07:21 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,21 +15,22 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/spllit-logo.png", 
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/spllit-logo.png", 
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/spllit-logo.png", 
+        type: "image/png",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/spllit-logo.png",
   },
 }
+
 
 export default function RootLayout({
   children,
